eac6a21fc9bd400f9dfbf93c8dfc5094efc0e076
Fa-Syaka-diouf <dfasyaka@ept.sn>
2025-02-11 11:27:59 +0000

    Ajout du module create-event et ses pages

--- src/app/create-event/reviser/reviser.component.ts
+++ src/app/create-event/reviser/reviser.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reviser',
   templateUrl: './reviser.component.html',
-  styleUrl: './reviser.component.css'
+  styleUrl: './reviser.component.css',
 })
-export class ReviserComponent {
-
-}
+export class ReviserComponent {}
